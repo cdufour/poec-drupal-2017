9126ef6d2fbd92dbc4bcf1756d313da65a31eb67
chris <opusidea@gmail.com>
2018-01-08 14:06:59 +0100

    Drupal dev j1

---
+++ drupal-dev/modules/custom/intro/src/Controller/IntroController.php
@@ -0,0 +1,58 @@
+<?php
+namespace Drupal\intro\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Controller de test
+ */
+class IntroController extends ControllerBase {
+
+  private $message = "Meilleurs voeux !";
+
+  /**
+   * Méthode coucou
+   *
+   * @return array
+   */
+  public function coucou() {
+    // renvoie d'un tableau associatif (render array) avec certaines
+    // clés permettant au système de rendu de Drupal
+    // de créer/composer la réponse à retourner au client
+
+    $res = '';
+    $res .= '<ul>';
+    $res .= '<li>A</li>';
+    $res .= '<li>B</li>';
+    $res .= '<li>C</li>';
+    $res .= '</ul>';
+
+    return [
+      '#markup' => $res,
+      'perso' => 'ce que je veux'
+    ];
+
+  }
+
+  /**
+   * Méthode list
+   *
+   * @return array
+   */
+  public function greet() {
+    $message = "";
+    $output = '';
+    if ($message != '') {
+      $output = $message;
+    } else {
+      $output = $this->message;
+    }
+
+    return [
+      '#markup' => $output
+    ];
+
+  }
+
+
+}
