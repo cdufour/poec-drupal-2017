d7b26202eea7518c13e718943ff21d373e014883
chris <opusidea@gmail.com>
2018-01-11 15:37:52 +0100

    drupal dev J4 pm break

--- drupal-dev2/web/modules/custom/proverb/src/Controller/ProverbController.php
+++ drupal-dev2/web/modules/custom/proverb/src/Controller/ProverbController.php
@@ -10,17 +10,57 @@ class ProverbController extends ControllerBase {
   public function list() {
 
     // retourne un objet modélisant le node id 1
-    $node = \Drupal\node\Entity\Node::load(9);
-
+    $node = \Drupal\node\Entity\Node::load(4);
     $title = $node->getTitle();
     // équivalent à:
     $titlebis = $node->title->value;
     // ->title renvoie un objet, ->value renvoie la valeur brut
-
     $body = $node->body->value;
 
+    //dpm($body);
+
+    // utilisation d'un service d'interrogation de la base de données
+    // (limité aux entités: node, user, term, ...)
+    $query = \Drupal::service('entity.query')->get('node');
+
+    // construction d'une requête avec condition/filtre
+    $query->condition('type', 'proverb');
+
+    // $nids reçoit un tableau d'identifiants (des entiers)
+    $nids = $query->execute();
+
+    // $proverbs reçoit un tableau d'objets de type NodeInterface
+    $proverbs = \Drupal\node\Entity\Node::loadMultiple($nids);
+
+    // sortie client
+    $output = '<ul>';
+    foreach($proverbs as $proverb) {
+      //dpm($proverb->getTitle());
+      $output .= '<li>'. $proverb->getTitle() .'</li>';
+    }
+    $output .= '</ul>';
+
+    // Interrogation des Users *******************
+    $query2 = \Drupal::service('entity.query')->get('user');
+    $query2->condition('status', 1); // filtre => on cible les actifs
+    $uids = $query2->execute();
+    $users = \Drupal\user\Entity\User::loadMultiple($uids);
+    foreach($users as $user) {
+      dpm($user->getUsername());
+      if ($user->isActive() == 1) {
+        //dpm('Actif');
+      } else {
+        //dpm('Bloqué');
+      }
+      //dpm($user->getRoles());
+      if ($user->hasRole('administrator')) {
+        //dpm('Je suis admin');
+      }
+    }
+    //********************************************
+
     return [
-      '#markup' => '<h2>' . $title . '</h2>' . $body
+      '#markup' => '<h2>' . $title . '</h2>' . $output
     ];
   }
 
